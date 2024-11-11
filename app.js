d5a0453235b78d487d5209f272440e16f5dbb106
sohelhussain <sohelhussaing@gmail.com>
2024-11-11 23:04:29 +0530

    feat: ADDING A LOGGER

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ require('./models/db')
 const indexRouter = require('./router/indexRouter')
 require('./models/db').connectDatabase();
 
+//logger
+const logger = require('morgan');
+app.use(logger("short"));
+
 app.use("/api/v1",indexRouter)
 
 app.listen(8000)
