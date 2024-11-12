07b83a3ca738c23bd9074683c3353fde8c9cfd5d
sohelhussain <sohelhussaing@gmail.com>
2024-11-12 08:17:15 +0530

    wip: MAIL & USERCHANGING PASSWORD

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const {userController} = require('../controller/userController');
+const { userController, userSignup, userSignin, userSignout, usersendmail, userforgetlink, userresetpassword} = require('../controller/userController');
+const { isAuthenticated } = require('../middleware/auth');
 
-router.get('/',userController);
+
+
+router.get('/', userController);
+// router.post()
+
+router.post("/user/signup", userSignup);
+router.post("/user/signin", userSignin);
+router.get("/user/signout", isAuthenticated, userSignout);
+router.post("/user/send-mail", usersendmail);
+router.post("/user/forgot-password-link/:userId", userforgetlink);
+router.post("/user/reset-password/:userId", isAuthenticated, userresetpassword);
+
 
 module.exports = router;
