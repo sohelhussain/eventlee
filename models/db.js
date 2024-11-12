c3c3a5cffa09f3eaa1f5e6ebfa094cc1a256cddb
sohelhussain <sohelhussaing@gmail.com>
2024-11-12 07:04:43 +0530

    wip: USERMODEL

--- models/db.js
+++ models/db.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 exports.connectDatabase = async () => {
     try{
         await mongoose.connect(process.env.MONGO_DB)
-        console.log('connect');
         console.log('db connection established');
     }catch(err){
         console.log(err.message);
