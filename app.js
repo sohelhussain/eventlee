f061f301a3192054c2b130c15636e88df91a932d
sohelhussain <sohelhussaing@gmail.com>
2024-11-12 09:47:57 +0530

    feat SESSION ADD

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-require('./models/db')
-const indexRouter = require('./router/indexRouter')
-require('./models/db').connectDatabase();
+const indexRouter = require("./router/indexRouter");
+require("./models/db").connectDatabase();
+const expressSession = require("express-session");
+const cookieParser = require("cookie-parser");
 
-//logger
-const logger = require('morgan');
+
+const logger = require("morgan");
 app.use(logger("short"));
+const ErrorHandler = require("./utils/ErrorHandler");
 
-app.use("/api/v1",indexRouter)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(8000)
+app.use(
+  expressSession({
+    resave: true,
+    saveUninitialized: true,
+    secret: process.env.SESSION_SECRET,
+  })
+);
+
+app.use(cookieParser());
+
+app.use("/api/v1", indexRouter);
+app.all("*", (req, res, next) => {
+  next(new ErrorHandler(`request url not found: ${req.url}`), 404);
+});
+app.use(generatedError);
+
+app.listen(8000);
